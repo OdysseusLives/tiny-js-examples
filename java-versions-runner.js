7cfcffe3efce1acba4004e3d4c3ce2919a52bd27
Aubrey Chipman <achipman@netflix.com>
2018-08-08 09:15:29 -0700

    Improve usage

--- java-versions-runner.js
+++ java-versions-runner.js
@@ -1,9 +1,11 @@
 #! /usr/bin/env node
 
-// Example usage: ./java-versions-runner.js -a 1.8.0_111 -b 10.0.2
-
 var javaVersions = require('./lib/java-versions.js');
-var argv = require('yargs').argv;
+var argv = require('yargs')
+  .usage('Usage: $0 [first-java-version] [second-java-version]\ni.e. "$0 1.8.0_111 10.0.2"')
+  .demandCommand(2)
+  .argv;
 
-console.log('a: %s, b: %s', argv.a, argv.b);
-console.log(javaVersions(argv.a, argv.b, 0))
+
+console.log('first: %s, second: %s', argv._[0], argv._[1]);
+console.log(javaVersions(argv._[0], argv._[1], 0))
